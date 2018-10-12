6b3450fb39d6f9ccd5a3875d911eec60c627cdab
Joolon <179777072@qq.com>
2018-10-12 19:57:19 +0800

    Update Libs/ExcelFile.class.php

--- Libs/ExcelFile.class.php
+++ Libs/ExcelFile.class.php
@@ -3,7 +3,7 @@ namespace Libs;
 
 /**
  * Created by JoLon.
- * EXCEL：数据导入导出
+ * EXCEL：数据导入导出，相关处理方法
  * User: JoLon
  * Date: 2018/09/04
  * Time: 18:55
@@ -15,7 +15,7 @@ class ExcelFile
      * 读取EXCEL文件所有数据
      * @return array
      */
-    public function import(){
+    public static function import(){
         $files      = $_FILES;
 
         $file_name  = $files['name']['file_execl'];
@@ -28,6 +28,7 @@ class ExcelFile
             exit('只能导入EXCEL文件');
         }
 
+        // 此处 导入 EXCEL 类文件
         if ($fileExp == 'xls')  $PHPReader = new \PHPExcel_Reader_Excel5();
         if ($fileExp == 'xlsx') $PHPReader = new \PHPExcel_Reader_Excel2007();
 
@@ -50,6 +51,22 @@ class ExcelFile
         return $sheetData;
 
     }
+	
+    /**
+     * EXCEL 数据列的序号(数字)转成 EXCEL 列字母
+     * @param int	    $index  当前列索引（从0开始 如A:0,B:1,C:3,...,Z:25,AZ:26,AB:27...）
+     * @return string
+     */
+    public static function getColumnName($index){
+        $offset = 65;// 偏移量
+
+        $str = '';
+        if (floor($index / 26) > 0) {
+            $str .= self::getColumnName(floor($index / 26)-1);
+        }
+        return $str . chr($index % 26 + $offset);
+    }
+	
 
 
 }
