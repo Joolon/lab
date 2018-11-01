025f42258dcd138bf80c1bc2baf8f9d1bb9f5860
Joolon <179777072@qq.com>
2018-11-01 20:07:04 +0800

    验证日期

--- Libs/StringTool.class.php
+++ Libs/StringTool.class.php
@@ -38,6 +38,36 @@ class StringTool
     }
 
     /**
+     * 从一个字符串中提取第一个日期并验证日期的合法性
+     * @param string $text  目标字符串
+     * @return bool|string  string.匹配到的日期  false.没有日期或日期不合法
+     */
+    public function getDateFromStringCheck($text){
+        $pattern = "/\d{4}((-|\.|\/)?\d{1,2}){2}/";
+        preg_match($pattern,$text,$match);
+
+        if(empty($match)) return false;
+
+        $date       = $match[0];
+        if(strpos($date,'.') !== false){
+            $date_break = explode('.',$date);
+        }elseif(strpos($date,'-') !== false){
+            $date_break = explode('-',$date);
+        }else{
+            $date_break = array();
+            $date_break[0] = substr($date,0,4);
+            $date_break[1] = substr($date,4,2);
+            $date_break[2] = substr($date,6,2);
+        }
+        $result     = checkdate($date_break[1],$date_break[2],$date_break[0]);// 验证日期是否合法
+        if($result){
+            return $date;
+        }else{
+            return false;
+        }
+    }
+
+    /**
      * 判断字符串中是否含有中文
      * @param $content
      * @return bool
