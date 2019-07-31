f663a937b4e6f9f7068ad03457bd8675eacda894
Joolon <179777072@qq.com>
2019-07-31 21:19:30 +0800

    ConsumerClient1
    ConsumerClient1

--- Demo/MyRabbitMQ/test_consumer.php
+++ Demo/MyRabbitMQ/test_consumer.php
@@ -5,20 +5,23 @@ set_time_limit(0);
 include_once '../../index.php';
 
 
-class A{
+/**
+ * Class ConsumerClient1
+ */
+class ConsumerClient1{
 
-    function processMessage($envelope, $queue) {
+    public function processMessage($envelope, $queue) {
         $msg = $envelope->getBody();
         $envelopeID = $envelope->getDeliveryTag();
-        file_put_contents("d:/log123.log", $msg.'|'.$envelopeID.PHP_EOL,FILE_APPEND);
+        file_put_contents("d:/log123.log", date('Y-m-d H:i:s')."\r ".$msg.'|'.$envelopeID.PHP_EOL,FILE_APPEND);
         $queue->ack($envelopeID);
     }
 }
 
-$a = new A();
+$client1 = new ConsumerClient1();
 
 try{
-    (new ConsumerMQ)->run(array($a,'processMessage'),false);
+    (new ConsumerMQ)->run(array($client1,'processMessage'),false);
 }catch(\Exception $exception){
     var_dump($exception->getMessage());
 }
