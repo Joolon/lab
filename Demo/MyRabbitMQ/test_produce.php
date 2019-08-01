c15ab2aed65777b42d8c0f3bac2a4263af7161a2
Joolon <179777072@qq.com>
2019-08-01 13:46:12 +0800

    rabbitmq produce

--- Demo/MyRabbitMQ/test_produce.php
+++ Demo/MyRabbitMQ/test_produce.php
@@ -3,12 +3,13 @@ use \Libs\MyRabbitMQ\ProduceMQ;
 
 include_once '../../index.php';
 
-$conf     = require 'config.php';
-$exchange = 'amq.direct';
-$message  = 'product message '.rand(10000, 99999);
+$rb_conf     = require 'config.php';
+$rb_exchange = 'amq.direct';
+$rb_msg      = 'product message '.rand(10000, 99999);
+$rb_route    = '111111';
 
 try{
-    (new ProduceMQ($conf['host'],$exchange))->send($message,'111111');
+    (new ProduceMQ($rb_conf['host'],$rb_exchange))->send($rb_msg,$rb_route);
 }catch(\Exception $exception){
     var_dump($exception->getMessage());
 }
