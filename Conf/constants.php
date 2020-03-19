564c55458cb8a880532a534a61bbeeca22ee605f
Joolon <179777072@qq.com>
2020-03-19 16:55:13 +0800

    swoole

---
+++ Conf/constants.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JoLon.
+ * User: JoLon
+ * Date: 2016/10/14
+ * Time: 11:51
+ */
+
+
+defined('SWOOLE_SERVER') OR define('SWOOLE_SERVER','172.18.235.99');//swoole本地环境
--- Demo/swoole/http_server.php
+++ Demo/swoole/http_server.php
@@ -1,11 +1,19 @@
 <?php
 
-$http = new Swoole\Http\Server("0.0.0.0", 9501);
+include_once dirname(dirname(dirname(__FILE__))).DIRECTORY_SEPARATOR.'Conf/constants.php';
+
+$http = new Swoole\Http\Server(SWOOLE_SERVER, 9501);
 
 $http->on('request', function ($request, $response) {
     var_dump($request->get, $request->post);
     $response->header("Content-Type", "text/html; charset=utf-8");
     $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
+});
+
+$http->on('request', function ($request, $response) {
+    list($controller, $action) = explode('/', trim($request->server['request_uri'], '/'));
+    //根据 $controller, $action 映射到不同的控制器类和方法
+    (new $controller)->$action($request, $response);
 });
 
 $http->start();
