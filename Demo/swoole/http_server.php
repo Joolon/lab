9012b0477f6557b68d92173bdeb7dd149f01b668
Joolon <179777072@qq.com>
2020-03-20 09:06:06 +0800

    Update http_server.php

--- Demo/swoole/http_server.php
+++ Demo/swoole/http_server.php
@@ -10,10 +10,4 @@ $http->on('request', function ($request, $response) {
     $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
 });
 
-$http->on('request', function ($request, $response) {
-    list($controller, $action) = explode('/', trim($request->server['request_uri'], '/'));
-    //根据 $controller, $action 映射到不同的控制器类和方法
-    (new $controller)->$action($request, $response);
-});
-
 $http->start();
