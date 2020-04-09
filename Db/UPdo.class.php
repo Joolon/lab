565aeee3c0f4ca80663e36a415a8c3b8bde4b274
Joolon <179777072@qq.com>
2020-04-09 19:58:44 +0800

    pdo sqlite

--- Db/UPdo.class.php
+++ Db/UPdo.class.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Db;
 
 /**
@@ -7,38 +8,31 @@ namespace Db;
  * Date: 2016/8/29
  * Time: 11:31
  */
+class UPdo {
 
-class UPdo extends \PDO
-{
-
-    private static $_dbms = 'mysql';
+    private static $_dbms     = 'mysql';
     private static $_instance = null;
-    private $sql = null;
 
-    public function __construct()
-    {
-        $host = C('DB_HOST');
-        $dbName = C('DB_NAME');
-        $username = C('DB_USER');
-        $passwd = C('DB_PWD');
+    private function __construct(){
+        // TODO: Implement __construct() method.
+    }
 
-        $dsn = self::$_dbms.":host=$host;dbname=$dbName";
-
-        parent::__construct($dsn, $username, $passwd);
+    private function __clone(){
+        // TODO: Implement __clone() method.
     }
 
     public static function getInstance(){
+        if(!self::$_instance instanceof \PDO){
+            $host     = C('DB_HOST');
+            $dbName   = C('DB_NAME');
+            $username = C('DB_USER');
+            $passwd   = C('DB_PWD');
 
-        if( !self::$_instance instanceof self){
-            self::$_instance = new self;
-            var_dump(self::$_instance);
-
+            $dsn = self::$_dbms.":host=$host;dbname=$dbName";
+            self::$_instance = new \PDO($dsn, $username, $passwd);
         }
+
         return self::$_instance;
     }
-
-//    public function
-
-
 
 }
