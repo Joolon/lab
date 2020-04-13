57bd4710effecd2f85289038c84edf07318cdfc4
Joolon <179777072@qq.com>
2020-04-13 16:46:41 +0800

    mysqli  pdo sqlite3

--- Demo/Database/pdo.php
+++ Demo/Database/pdo.php
@@ -2,8 +2,8 @@
 use Db\UPdo;
 
 /**
- * SQLite：SQLite是一个软件库，实现了自给自足的、无服务器的、零配置的、事务性的 SQL 数据库引擎。
- *
+ * SQLite3：
+ * SQLite3：SQLite是一个软件库，实现了自给自足的、无服务器的、零配置的、事务性的 SQL 数据库引擎。
  *      经济性：无需单独的数据库服务器，低成本，于对并发数量要求不高的本地服务
  *      效率性：占用资源很小
  *      可靠性：数据存储在本地服务器上，数据库就是一个文件，省去了网络连接开销
@@ -12,24 +12,31 @@ use Db\UPdo;
  *
  * 主要应用于小客户量访问（百万次以下）的WEB服务、手机、 PDA 、机顶盒、嵌入式程序、物联网以及其他独立的客户端程序
  *
- *
  * SQLite适用于小型网站、客户端；MySQL/Oracle等C/S数据库一般用来提供大数据、高并发的数据服务。
- */
-
-/**
- * PDO
+ *
+ *
+ * PDO：
  * PHP 数据对象 （PDO） 扩展为PHP访问数据库定义了一个轻量级的一致接口。
  * PDO 提供了一个数据访问抽象层，这意味着，不管使用哪种数据库，都可以用相同的函数（方法）来查询和获取数据。
+ *
+ *
+ * MySQLi：
+ * MySQLi 是 MySQL 的一个升级版本；MySQL是非永久性连接，每次连接都会打开一个新连接；MySQLi 是永久性连接，每次打开会使用连接池中的连接，可以减轻服务器的压力。
+ * 永久性链接就是连接池，建立连接之后同一服务器同一用户访问同一数据库就会使用连接池中的连接，不会重新建立连接。
+ *      （如果有大量用户访问就要增加 max_connections 参数，如果是 mysql_connect 非永久性连接就不用考虑这个参数了。）
+ * MySQLi 可以支持 面向过程和面向对象的方式操作数据库，MySQL 只支持面向过程。
+ *
  */
 
 
-$uPDO = UPdo::getInstance();
+//$uPDO = UPdo::getInstance();
+//$pdoStatement = $uPDO->query('SELECT * from pur_order_old limit 4');
+//
+//print_r($pdoStatement->fetchAll(\PDO::FETCH_ASSOC));exit;
 
-$uPDO = UPdo::getInstance();
-$uPDO = UPdo::getInstance();
-
-foreach ($uPDO->query('SELECT * from pur_order_old limit 2') as $row) {
-    print_r($row); //你可以用 echo($GLOBAL); 来看到这些值
-}
-
-print_r($uPDO);
+//$conn = new mysqli('localhost', 'root', 'root', 'demo');
+//$sql = "select * from pur_order_old limit 2";
+//$query = $conn->query($sql);
+//while($row = $query->fetch_array()){
+//    print_r($row);
+//}
